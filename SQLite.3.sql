dacabb00839a216a7997a86422462087c1c1a2ae
enzocetrapereira-wq <enzo.cetra.pereira@escola.pr.gov.br>
2026-03-17 22:56:29 -0300

    Add files via upload

---
+++ SQLite.3.sql
@@ -0,0 +1,6 @@
+INSERT into Produtos (nome, preco, quantidade)
+VALUES ('Mouse', 59.99, 36),
+('Teclado', 89.99, 45),
+('Monitor', 499.99, 105),
+('Fone de Ouvido', 20.99, 14),
+('Webcam', 54.99, 62)
---
+++ SQLite.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Produtos (
+codigo integer PRIMARY KEY AUTOINCREMENT,
+  nome text,
+  preco real,
+  quantidade integer
+  );
